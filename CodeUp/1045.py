0e98b18837ea62e7076084c2d9ccde5964e88abf
이종혁 <leejh0317@kyungshin.co.kr>
2020-12-21 14:48:39 +0900

    Solved

---
+++ CodeUp/1045.py
@@ -0,0 +1,15 @@
+# 1045 : [기초-산술연산] 정수 2개 입력받아 자동 계산하기
+a, b = map(int, input().split())
+
+# 합
+print(a+b)
+# 차
+print(a-b)
+# 곱
+print(a*b)
+# 몫
+print(a//b)
+# 나머지
+print(a%b)
+# 나눈 값
+print('%.2f' %(a/b))
