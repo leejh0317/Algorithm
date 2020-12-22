b1cd2a3cba7b44c751dc85845309352f9aa2e13c
이종혁 <leejh0317@kyungshin.co.kr>
2020-12-22 14:51:53 +0900

    Solved

--- CodeUp/1084.py
+++ CodeUp/1084.py
@@ -1,1 +1,11 @@
 # 1084 : [기초-종합] 빛 섞어 색 만들기(설명)
+a, b, c = map(int, input().split())
+cnt = 0
+
+for i in range(a):
+    for j in range(b):
+        for k in range(c):
+            print('{} {} {}'.format(i, j, k))
+            # print(f'{i} {j} {k}') python 3.6 이상
+            cnt += 1
+print(cnt)
