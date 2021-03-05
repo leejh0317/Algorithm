2e32eb60d0c135afc8451583958cacb1c2f01768
이종혁 <leejh0317@kyungshin.co.kr>
2021-03-05 12:39:01 +0900

    Solved

---
+++ CodeUp/Python 기초 100제/6012.py
@@ -0,0 +1,6 @@
+# [기초-입출력] 정수 2개 입력받아 그대로 출력하기1(설명)(py)
+a = int(input())
+b = int(input())
+
+print(a)
+print(b)
---
+++ CodeUp/Python 기초 100제/6015.py
@@ -0,0 +1,3 @@
+# [기초-입출력] 정수 2개 입력받아 그대로 출력하기2(설명)(py)
+a, b = map(int, input().split())
+print(a, b, sep='\n')
